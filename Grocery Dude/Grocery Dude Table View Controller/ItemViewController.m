825830b63daa968d912f18b64433e4bc4aee93da
birneysky <birneysky@163.com>
2016-02-18 17:39:38 +0800

    modify for ItemViewController

--- Grocery Dude/Grocery Dude Table View Controller/ItemViewController.m
+++ Grocery Dude/Grocery Dude Table View Controller/ItemViewController.m
@@ -11,7 +11,7 @@
 #import "AppDelegate.h"
 #import "Item+CoreDataProperties.h"
 
-@interface ItemViewController ()
+@interface ItemViewController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
@@ -28,6 +28,21 @@
 }
 
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self refreshInterface];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+      CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
+    [cdh saveContext];
+}
+
+
+
 #pragma mark - *** Helper ***
 
 - (void)hideKeyboardWhenBackgroundIsTapped
@@ -42,9 +57,49 @@
     [self.view endEditing:YES];
 }
 
+- (void)refreshInterface
+{
+    if (self.selectedItemID) {
+        CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
+        Item* item = [cdh.context existingObjectWithID:self.selectedItemID error:nil];
+        self.nameTextField.text = item.name;
+        self.quantityTextField.text = item.quantity.stringValue;
+    }
+}
+
+#pragma mark - *** Target Action ***
 - (IBAction)done:(id)sender {
     
+    [self hideKeyboard];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark - *** TextField Delegate ***
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    if (textField == self.nameTextField){
+        if ([self.nameTextField.text isEqualToString:@"New Item"]) {
+            self.nameTextField.text = @"";
+        }
+    }
+}
+
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
     
+    Item* item = [cdh.context existingObjectWithID:self.selectedItemID error:nil];
+    
+    if (textField == self.nameTextField) {
+        if ([self.nameTextField.text isEqualToString:@""]) {
+            self.nameTextField.text = @"New Item";
+        }
+        item.name = self.nameTextField.text;
+    }
+    else if (textField == self.quantityTextField){
+        item.quantity = [NSNumber numberWithFloat:self.quantityTextField.text.floatValue];
+    }
 }
 
 
