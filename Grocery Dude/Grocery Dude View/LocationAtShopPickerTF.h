793df2c815defecffb44713f7c02c0137d41ce73
birneysky <birneysky@163.com>
2016-02-23 12:40:24 +0800

    add LocationAtShopPickerTF

---
+++ Grocery Dude/Grocery Dude View/LocationAtShopPickerTF.h
@@ -0,0 +1,13 @@
+//
+//  LocationAtShopPickerTF.h
+//  Grocery Dude
+//
+//  Created by zhangguang on 16/2/23.
+//  Copyright © 2016年 com.v2tech. All rights reserved.
+//
+
+#import "CoreDataPickerTF.h"
+
+@interface LocationAtShopPickerTF : CoreDataPickerTF
+
+@end
