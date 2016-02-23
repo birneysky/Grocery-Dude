a6f385b1ce19baa9377681138ed341d46574971f
birneysky <birneysky@163.com>
2016-02-23 13:09:18 +0800

    modify LocationAtHomePickerTF.m LocationAtShopPickerTF.m

--- Grocery Dude/Grocery Dude View/LocationAtHomePickerTF.m
+++ Grocery Dude/Grocery Dude View/LocationAtHomePickerTF.m
@@ -7,15 +7,39 @@
 //
 
 #import "LocationAtHomePickerTF.h"
+#import "LocationAtHome+CoreDataProperties.h"
+#import "AppDelegate.h"
 
 @implementation LocationAtHomePickerTF
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - *** Helper ***
+- (void)fetch
+{
+    CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
+    NSFetchRequest* request = [NSFetchRequest fetchRequestWithEntityName:@"LocationAtHome"];
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]];
+    [request setFetchBatchSize:50];
+    NSError* erro = nil;
+    self.pickerData = [cdh.context executeFetchRequest:request error:&erro];
+    if (erro) {
+        DebugLog(@"Error populating picker %@, %@",erro,erro.localizedDescription);
+    }
+    [self selectDefaultRow];
 }
-*/
+
+- (void)selectDefaultRow
+{
+    if (self.selectObjectID && self.pickerData.count) {
+        CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
+        LocationAtHome* selectObject = [cdh.context existingObjectWithID:self.selectObjectID error:nil];
+        [self.pickerData enumerateObjectsUsingBlock:^(LocationAtHome*  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if ([obj.storedin compare: selectObject.storedin] == NSOrderedSame) {
+                [self.picker selectRow:idx inComponent:0 animated:NO];
+                [self.pickerDelegate selectedObjectID:[selectObject objectID] changedForPickerTF:self];
+                *stop = YES;
+            }
+        }];
+    }
+}
 
 @end
--- Grocery Dude/Grocery Dude View/LocationAtShopPickerTF.m
+++ Grocery Dude/Grocery Dude View/LocationAtShopPickerTF.m
@@ -7,15 +7,38 @@
 //
 
 #import "LocationAtShopPickerTF.h"
+#import "LocationAtShop+CoreDataProperties.h"
+#import "AppDelegate.h"
 
 @implementation LocationAtShopPickerTF
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)fetch
+{
+    CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
+    NSFetchRequest* request = [NSFetchRequest fetchRequestWithEntityName:@"LocationAtShop"];
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"aisle" ascending:YES]];
+    NSError* error = nil;
+    self.pickerData = [cdh.context executeFetchRequest:request error:&error];
+    
+    if (error) {
+        DebugLog(@"Error populating picker %@, %@",error,error.localizedDescription);
+    }
+    [self selectDefaultRow];
 }
-*/
+
+- (void)selectDefaultRow
+{
+    if (self.selectObjectID && self.pickerData.count > 0) {
+        CoreDataHelper* cdh = [(AppDelegate*)[[UIApplication sharedApplication] delegate] coreDataHelper];
+        LocationAtShop* selectObject = [cdh.context existingObjectWithID:self.selectObjectID error:nil];
+        [self.pickerData enumerateObjectsUsingBlock:^(LocationAtShop*  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if ([obj.aisle compare:selectObject.aisle ] == NSOrderedSame) {
+                [self.picker selectRow:idx inComponent:0 animated:NO];
+                [self.pickerDelegate selectedObjectID:self.selectObjectID changedForPickerTF:self];
+                *stop = YES;
+            }
+        }];
+    }
+}
 
 @end
