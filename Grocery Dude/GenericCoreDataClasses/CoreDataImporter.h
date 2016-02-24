e0ce280225bd3c2413b627fa1bc086287b24e85a
birneysky <birneysky@163.com>
2016-02-24 16:50:13 +0800

    add CoreDataImporter

---
+++ Grocery Dude/GenericCoreDataClasses/CoreDataImporter.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataImporter.h
+//  Grocery Dude
+//
+//  Created by zhangguang on 16/2/24.
+//  Copyright © 2016年 com.v2tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataImporter : NSObject
+
+@end
---
+++ Grocery Dude/GenericCoreDataClasses/CoreDataImporter.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataImporter.m
+//  Grocery Dude
+//
+//  Created by zhangguang on 16/2/24.
+//  Copyright © 2016年 com.v2tech. All rights reserved.
+//
+
+#import "CoreDataImporter.h"
+
+@implementation CoreDataImporter
+
+@end
